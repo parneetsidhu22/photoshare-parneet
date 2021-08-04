6af93e9fb07eb5fbb73ae91f535b1848cdfbc71d
Parneet Sidhu <parneetsidhu2004@outlook.com>
2021-08-04 18:00:29 +0530

    Some fixes

--- user_authentication/models.py
+++ user_authentication/models.py
@@ -4,7 +4,7 @@ from sorl.thumbnail import ImageField
 class Profile(models.Model):
     username = models.CharField(max_length=100)
     image = ImageField(upload_to="photoshare_image/profile",
-        default="https://res.cloudinary.com/parneetsingh/image/upload/v1/photoshare_image/profile/blank-profile-picture-973460_640_gu5g1r",
+        default="https://res.cloudinary.com/parneetsingh/image/upload/v1628080124/photoshare_image/profile/blank-profile-picture-973460_1280_szji3x.png",
         blank=True
     )
     disabled = models.BooleanField(default=False)
