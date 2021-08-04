7bfd0ac47e31b17fd1bb14bb788ca4303e7ca62c
Parneet Sidhu <parneetsidhu2004@outlook.com>
2021-08-04 16:01:58 +0530

    Debugging

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2tl=*!02)84ojuo26g*(nrw2e_7m^%(0v8@i6@r$ke(y&6xs34'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
+ALLOWED_HOSTS = ["projectphotoshare.herokuapp.com"]
+'''
 if DEBUG == True:
     ALLOWED_HOSTS = ["192.168.1.23","127.0.0.1"]
 else:
-    ALLOWED_HOSTS = ["projectphotoshare.herokuapp.com"]
+    ALLOWED_HOSTS = ["projectphotoshare.herokuapp.com"]'''
     
 
 # Application definition
